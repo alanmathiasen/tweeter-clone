3e1a358a79ee1fc3721cec84e13840e772e497e6
alanmathiasen <alanemathiasen@gmail.com>
2023-04-06 15:30:14 -0300

    changes

--- src/pages/TweetPage/index.js
+++ src/pages/TweetPage/index.js
@@ -15,97 +15,97 @@ const TweetPage = () => {
     const [parents, setParents] = useState([]);
     const tweetRef = doc(db, "tweets", id);
     //TODO use tweetCrud fn
-    const getTweet = async (idTweet) => {
-        //const tweetRef = doc(db, "tweets", idTweet);
-        const tweetSnap = await getDoc(tweetRef);
-        if (tweetSnap.exists()) {
-            const twit = { ...tweetSnap.data() };
-            return twit;
-        } else {
-            return "ERROR";
-        }
-    };
+    // const getTweet = async (idTweet) => {
+    //     //const tweetRef = doc(db, "tweets", idTweet);
+    //     const tweetSnap = await getDoc(tweetRef);
+    //     if (tweetSnap.exists()) {
+    //         const twit = { ...tweetSnap.data() };
+    //         return twit;
+    //     } else {
+    //         return "ERROR";
+    //     }
+    // };
 
-    const getChildren = async () => {
-        //const tweetRef = doc(db, "tweets", idState);
-        const tweetSnap = await getDoc(tweetRef);
-        let childs = [];
+    // const getChildren = async () => {
+    //     //const tweetRef = doc(db, "tweets", idState);
+    //     const tweetSnap = await getDoc(tweetRef);
+    //     let childs = [];
 
-        if (tweetSnap.exists() && tweetSnap.data().children) {
-            childs = await Promise.all(
-                tweetSnap.data().children.map(async (idChild) => {
-                    const newChild = await getTweet(idChild);
-                    if (newChild !== "ERROR") newChild.id = idChild;
-                    return newChild;
-                })
-            );
-        } else {
-        }
+    //     if (tweetSnap.exists() && tweetSnap.data().children) {
+    //         childs = await Promise.all(
+    //             tweetSnap.data().children.map(async (idChild) => {
+    //                 const newChild = await getTweet(idChild);
+    //                 if (newChild !== "ERROR") newChild.id = idChild;
+    //                 return newChild;
+    //             })
+    //         );
+    //     } else {
+    //     }
 
-        const lastChilds = childs.filter((child) => {
-            if (child === "ERROR") {
-                return false;
-            }
-            return true;
-        });
-        lastChilds.sort((a, b) => (a.timestamp > b.timestamp ? 1 : -1));
-        return lastChilds;
-    };
+    //     const lastChilds = childs.filter((child) => {
+    //         if (child === "ERROR") {
+    //             return false;
+    //         }
+    //         return true;
+    //     });
+    //     lastChilds.sort((a, b) => (a.timestamp > b.timestamp ? 1 : -1));
+    //     return lastChilds;
+    // };
 
-    //setChildrensId con onSnapshot
-    useEffect(() => {
-        //const tweetRef = doc(db, "tweets", idState);
+    // //setChildrensId con onSnapshot
+    // useEffect(() => {
+    //     //const tweetRef = doc(db, "tweets", idState);
 
-        const unsubscribe = onSnapshot(tweetRef, (snap) => {
-            if (snap.data()) setChildrenIds(snap.data().children);
-            else setChildrenIds([]);
-        });
-        return () => unsubscribe();
-    }, [idState]);
+    //     const unsubscribe = onSnapshot(tweetRef, (snap) => {
+    //         if (snap.data()) setChildrenIds(snap.data().children);
+    //         else setChildrenIds([]);
+    //     });
+    //     return () => unsubscribe();
+    // }, [idState]);
 
-    const getParents = async () => {
-        const pIds = [];
-        const tweetSnap = await getDoc(tweetRef);
-        if (tweetSnap.data().parentId) {
-            await hasParent(idState, pIds);
-        }
+    // const getParents = async () => {
+    //     const pIds = [];
+    //     const tweetSnap = await getDoc(tweetRef);
+    //     if (tweetSnap.data().parentId) {
+    //         await hasParent(idState, pIds);
+    //     }
 
-        let parents = [];
-        if (pIds) {
-            parents = await Promise.all(
-                pIds.map(async (idP) => {
-                    const newChild = await getTweet(idP);
-                    if (newChild !== "ERROR") newChild.id = idP;
-                    return newChild;
-                })
-            );
-        }
+    //     let parents = [];
+    //     if (pIds) {
+    //         parents = await Promise.all(
+    //             pIds.map(async (idP) => {
+    //                 const newChild = await getTweet(idP);
+    //                 if (newChild !== "ERROR") newChild.id = idP;
+    //                 return newChild;
+    //             })
+    //         );
+    //     }
 
-        return parents.reverse();
-    };
+    //     return parents.reverse();
+    // };
 
-    const hasParent = async (id, pIds) => {
-        const tweetRef = doc(db, "tweets", id);
-        const tweetSnap = await getDoc(tweetRef);
-        if (tweetSnap.data().parentId) {
-            pIds.push(tweetSnap.data().parentId);
-            await hasParent(tweetSnap.data().parentId, pIds);
-        } else {
-            return 0;
-        }
-    };
+    // const hasParent = async (id, pIds) => {
+    //     const tweetRef = doc(db, "tweets", id);
+    //     const tweetSnap = await getDoc(tweetRef);
+    //     if (tweetSnap.data().parentId) {
+    //         pIds.push(tweetSnap.data().parentId);
+    //         await hasParent(tweetSnap.data().parentId, pIds);
+    //     } else {
+    //         return 0;
+    //     }
+    // };
 
-    useEffect(() => {
-        (async () => {
-            setChildren(await getChildren());
-            setParents(await getParents());
-        })();
-    }, [childrenIds]);
+    // useEffect(() => {
+    //     (async () => {
+    //         setChildren(await getChildren());
+    //         setParents(await getParents());
+    //     })();
+    // }, [childrenIds]);
 
-    //const location = useLocation();
-    useEffect(() => {
-        setIdState(id);
-    }, [id]);
+    // //const location = useLocation();
+    // useEffect(() => {
+    //     setIdState(id);
+    // }, [id]);
 
     return (
         <Wrapper>
