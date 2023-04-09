1f768fe87900cd0c56395b45ef816240cf1166f0
alanmathiasen <alanemathiasen@gmail.com>
2023-04-09 19:45:44 -0300

    small css change

--- src/pages/TweetPage/TweetPage.styles.js
+++ src/pages/TweetPage/TweetPage.styles.js
@@ -1,7 +1,5 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-  padding: 20px 0;
-  border-left: 1px solid lightgray;
-  border-right: 1px solid lightgray;
+    padding: 20px 0;
 `;
